d9e713130ca16617ba5c2901e184e0def4f0f4bd
Steve <112937622+Zaaaaat@users.noreply.github.com>
2023-09-27 10:22:21 +0200

    Question 2.1

--- src/app/dev-cart/page.tsx
+++ src/app/dev-cart/page.tsx
@@ -1,45 +1,36 @@
-"use client"
-import React, {useEffect, useState} from 'react';
-import {PRODUCTS_CATEGORY_DATA} from "tp-kit/data";
-import {Button, SectionContainer} from "tp-kit/components";
+"use client";
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import { Button, ProductCardLayout, SectionContainer } from "tp-kit/components";
 import {ProductCartLine} from "tp-kit/components/products";
 import {FormattedPrice} from "tp-kit/components/data-display";
+const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
 
 export default function DevCartPage() {
-    const products = PRODUCTS_CATEGORY_DATA[0].products.slice(0, 3);
-    const [totalPrice, setTotalPrice] = useState(0);
-
-    const calculateTotalPrice = () => {
-        let total = 0;
-        products.forEach((product) => {
-            total += product.price;
-        });
-        return total;
-    };
-
-    useEffect(() => {
-        const total = calculateTotalPrice();
-        setTotalPrice(total);
-    }, [products]);
-
     return (
         <SectionContainer
             className="py-36"
             wrapperClassName="flex flex-col lg:flex-row gap-24"
         >
+            {/* Produits */}
+            <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 flex-1">
+                {products.map((product) => (
+                    <ProductCardLayout
+                        key={product.id}
+                        product={product}
+                        button={<Button variant={"ghost"} fullWidth>Ajouter au panier</Button>}
+                    />
+                ))}
+            </section>
+            {/* /Produits */}
 
             {/* Panier */}
             <section className="w-full lg:w-1/3 space-y-8">
                 {products.map((product) => (
-                    <ProductCartLine
-                        product={product}
-                        qty={1}
-                        key={product.id}
-                    ></ProductCartLine>
+                    <ProductCartLine product={product} qty={1}></ProductCartLine>
                 ))}
 
-                <FormattedPrice price={totalPrice}></FormattedPrice>
-                <Button fullWidth> Valider le panier </Button>
+                Total: <FormattedPrice price={products.reduce((total, currentValue) => total = total + currentValue.price,0)}/>
+                <Button fullWidth>Commander</Button>
                 <Button variant={"outline"} fullWidth>Vider le panier</Button>
             </section>
             {/* /Panier */}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-import { ProductsCategoryData } from "tp-kit/types"
+import {ProductData, ProductsCategoryData} from "tp-kit/types"
 
 export type ProductFiltersResult = {
   categoriesSlugs : string[],
@@ -17,3 +17,12 @@ export type NextPageProps<T = Record<string, string>> = {
    */
   searchParams: { [key: string]: string | string[] | undefined }
 };
+
+export type ProductLineData = {
+  product: ProductData,
+  qty : number,
+};
+
+export type CartData = {
+  lines: ProductLineData[]
+}
